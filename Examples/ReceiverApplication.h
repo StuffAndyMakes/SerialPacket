8479b5b10c5bb18f4cc3714881484dd918d0e962
Andy <andy@StuffAndyMakes.com>
2015-05-20 07:45:19 -0700

    Licensing and Variable Naming

--- Examples/ReceiverApplication.h
+++ Examples/ReceiverApplication.h
@@ -1,16 +1,39 @@
 //
-//  Application.h
-//  ErrorCorrectionExperiment
+//  ReceiverApplication.cpp
+//  Error-Detecting Serial Packet Communications for Arduino Microcontrollers
+//  Originally designed for use in the Office Chairiot Mark II motorized office chair
 //
 //  Created by Andy Frey on 4/13/15.
-//  Copyright (c) 2015 Andy Frey. All rights reserved.
-//
+//  Copyright (c) 2015 Andy Frey/StuffAndyMakes.com. All rights reserved.
+/*
+The MIT License (MIT)
 
-#ifndef __ErrorCorrectionExperiment__Application__
-#define __ErrorCorrectionExperiment__Application__
+Copyright (c) 2015 Andy Frey/StuffAndyMakes.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef __ErrorDetection__ReceiverApplication__
+#define __ErrorDetection__ReceiverApplication__
 
 #include "Arduino.h"
-#include "Packet.h"
+#include "SerialPacket.h"
 
 
 typedef struct {
@@ -22,7 +45,7 @@ typedef struct {
 } Command;
 
 
-class Application: public PacketDelegate {
+class ReceiverApplication: public PacketDelegate {
     // instance variables
     uint64_t _expectedSerial;
     
@@ -35,7 +58,7 @@ public:
     static const uint8_t STATUS_NACK = 0;
     static const uint8_t STATUS_ACK = 1;
     
-    Application();
+    ReceiverApplication();
     void main();
     
     // packet delegate members
@@ -44,4 +67,4 @@ public:
 
 };
 
-#endif /* defined(__ErrorCorrectionExperiment__Application__) */
+#endif /* defined(__ErrorDetection__ReceiverApplication__) */
