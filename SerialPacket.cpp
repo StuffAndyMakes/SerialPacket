aae9209e5517a6d6dd0a581c18714aa72ff97030
Andy <andy@StuffAndyMakes.com>
2015-05-19 20:33:10 -0700

    Nixed the dynamic buffer allocation idea

--- SerialPacket.cpp
+++ SerialPacket.cpp
@@ -29,18 +29,13 @@ Packet::Packet() {
     _delegate = NULL;
     _sendingSerial = NULL;
     _receivingSerial = NULL;
-    _init();
-}
-
-void Packet::_init() {
+    _receiving = false;
     _state = STATE_NONE;
     _nextTimeout = 0;
     _dataPos = 0;
     _dataLength = 0;
     _crc = 0;
-    free(buffer);
-    buffer = NULL;
-    _receiving = false;
+    for (uint8_t i = 0; i < MAX_DATA_SIZE; i++) buffer[i] = 0;
 }
 
 void Packet::sendUsing(HardwareSerial *s) {
@@ -49,7 +44,6 @@ void Packet::sendUsing(HardwareSerial *s) {
 
 void Packet::receiveUsing(HardwareSerial *s) {
     _receivingSerial = s;
-    _init();
 }
 
 void Packet::setDelegate(PacketDelegate *d) {
@@ -97,12 +91,11 @@ uint8_t Packet::send(uint8_t *p, uint8_t l) {
         l = MAX_DATA_SIZE;
     }
     uint8_t bytesSent = 0;
-    _state = STATE_SENDING;
     _sendingSerial->write(FRAME_START); bytesSent++;
-    _dataLength = l;
-    _sendingSerial->write(_dataLength); bytesSent++;
     _crc = _crc8(p, l);
     _sendingSerial->write(_crc); bytesSent++;
+    _dataLength = l;
+    _sendingSerial->write(_dataLength); bytesSent++;
     uint8_t b = 0, dataCount = 0;
     for (b = 0; b < l; b++) {
         if ((p[b] == ESCAPE) || (p[b] == FRAME_START) || (p[b] == FRAME_END)) {
@@ -111,111 +104,103 @@ uint8_t Packet::send(uint8_t *p, uint8_t l) {
         _sendingSerial->write(p[b]); bytesSent++; dataCount++;
     }
     _sendingSerial->write(FRAME_END); bytesSent++;
-    _state = STATE_NONE;
     return bytesSent;
 }
 
 void Packet::startReceiving() {
+    if (_receiving == true || _receivingSerial == NULL) return;
+    _nextTimeout = millis() + _timeout;
+    _dataPos = 0;
+    _dataLength = 0;
+    _crc = 0;
+    for (uint8_t i = 0; i < MAX_DATA_SIZE; i++) buffer[i] = 0;
     _receiving = true;
+    _state = STATE_START_WAIT;
 }
 
 void Packet::stopReceiving() {
     _receiving = false;
+    _state = STATE_NONE;
 }
 
 void Packet::_callDelegateError(uint8_t err) {
-    _init();
     _delegate->didReceiveBadPacket(this, err);
-}
-
-void Packet::_addToBuffer(uint8_t b) {
-    buffer[_dataPos++] = b;
-    // data should end when the packet length field said it would
-    if (_dataPos >= _dataLength) {
-        _state = STATE_END_WAIT;
-    }
 }
 
 void Packet::loop() {
     
-    if (_receivingSerial == NULL || _receiving == false) return;
+    if (_receiving == false) return;
 
-    if (_receiving == true) {
+    while (_receivingSerial->available() > 0) {
+        _nextTimeout = millis() + _timeout;
+        
+        uint8_t c = (uint8_t)_receivingSerial->read();
 
-        if (_state != STATE_SENDING) {
+        switch (_state) {
 
-            if ((_receivingSerial->available() > 0) && (_state == STATE_NONE)) {
-                _state = STATE_START_WAIT;
-            }
-
-            while ((_state != STATE_NONE) && (_receivingSerial->available() > 0)) {
-                _nextTimeout = millis() + _timeout;
-                
-                int c = _receivingSerial->read();
-                if (c == -1) continue;
-
-                switch (_state) {
-                        
-                    case STATE_START_WAIT:
-                        if ((uint8_t)c == FRAME_START) {
-                            _state = STATE_LENGTH;
-                        }
-                        break;
-                        
-                    case STATE_LENGTH:
-                        _dataLength = (uint8_t)c;
-                        buffer = (uint8_t *)malloc(_dataLength);
-                        _state = STATE_CRC;
-                        break;
-                        
-                    case STATE_CRC:
-                        _crc = (uint8_t)c;
-                        _state = STATE_DATA;
-                        break;
-                        
-                    case STATE_DATA:
-                        if ((char)c == ESCAPE) {
-                            _state = STATE_ESCAPE;
-                        } else if ((uint8_t)c == FRAME_END) {
-                            // we should not be seeing this situation, so let delegate know and bomb out
-                            _callDelegateError(ERROR_LENGTH);
-                        } else {
-                            _addToBuffer((uint8_t)c);
-                        }
-                        break;
-                        
-                    case STATE_ESCAPE:
-                        _state = STATE_DATA; // this MUST go before _addToBuffer()
-                        _addToBuffer((uint8_t)c);
-                        break;
-                        
-                    case STATE_END_WAIT:
-                        if ((uint8_t)c == FRAME_END) {
-                            // check CRC and call delegate accordingly
-                            if (_crc == _crc8(buffer, _dataLength)) {
-                                _delegate->didReceivePacket(this);
-                                _state = STATE_NONE;
-                            } else {
-                                _callDelegateError(ERROR_CRC);
-                            }
-                        } else {
-                            // this is not the byte we're looking for
-                            Serial.println("FRAME_END (" + String(FRAME_END, DEC) + ") expected, got " + String(c, DEC));
-                            _callDelegateError(ERROR_FRAME);
-                        }
-                        break;
-                        
+            case STATE_START_WAIT:
+                if ((uint8_t)c == FRAME_START) {
+                    _state = STATE_CRC;
                 }
+                break;
                 
-            } // while (_receivingSerial->available() > 0)
-            
-            if (millis() > _nextTimeout) {
-                _callDelegateError(ERROR_TIMEOUT);
-            }
-            
-        } // if (_state != STATE_NONE) && (_state != STATE_SENDING))
+            case STATE_CRC:
+                _crc = c;
+                _state = STATE_LENGTH;
+                break;
+                
+            case STATE_LENGTH:
+                _dataLength = c;
+                if (_dataLength < 1) {
+                    _callDelegateError(ERROR_LENGTH);
+                } else {
+                    _state = STATE_DATA;
+                    _dataPos = 0;
+                }
+                break;
+                
+            case STATE_DATA:
+                if (c == ESCAPE) {
+                    _state = STATE_ESCAPE;
+                } else if ((c == FRAME_END) || (c == FRAME_START)) {
+                    // this should not happen, so clear everything and inform delegate
+                    _callDelegateError(ERROR_LENGTH);
+                } else {
+                    buffer[_dataPos++] = c;
+                }
+                break;
+                
+            case STATE_ESCAPE:
+                _state = STATE_DATA; // this MUST go 1st, in case _addToBuffer() changes state
+                buffer[_dataPos++] = c;
+                break;
+                
+            case STATE_END_WAIT:
+                if (c == FRAME_END) {
+                    // check CRC and call delegate accordingly
+                    if (_crc == _crc8(buffer, _dataLength)) {
+                        _delegate->didReceivePacket(this);
+                    } else {
+                        _callDelegateError(ERROR_CRC);
+                    }
+                } else {
+                    // this is not the byte we're looking for
+                    _callDelegateError(ERROR_FRAME);
+                }
+                _state = STATE_START_WAIT;
+                break;
+                
+        }
+    
+        // do we have all the bytes we're supposed to get?
+        if (_state == STATE_DATA && _dataPos >= _dataLength) {
+            _state = STATE_END_WAIT;
+        }
 
-    } // if (_receiving == true)
-    // wonder if we should be gobbling up incoming bytes to keep buffer empty?
+    } // while ((_state != STATE_NONE) && (_receivingSerial->available() > 0))
 
+    if (millis() > _nextTimeout && _state != STATE_NONE) {
+        _callDelegateError(ERROR_TIMEOUT);
+    }
+        
 }
