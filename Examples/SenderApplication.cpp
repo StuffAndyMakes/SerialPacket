52c219b8a62d73c08e0464b1b35a75005f7a0a19
Andy <andy@StuffAndyMakes.com>
2015-05-21 11:49:23 -0700

    Builds OK in Xcode

--- Examples/SenderApplication.cpp
+++ Examples/SenderApplication.cpp
@@ -6,30 +6,30 @@
 //  Created by Andy Frey on 4/13/15.
 //  Copyright (c) 2015 Andy Frey. All rights reserved.
 /*
-The MIT License (MIT)
+ The MIT License (MIT)
+ 
+ Copyright (c) 2015 Andy Frey/StuffAndyMakes.com
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 
-Copyright (c) 2015 Andy Frey/StuffAndyMakes.com
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
-#include "Application.h"
+#include "SenderApplication.h"
 #include "SerialPacket.h"
 
 
@@ -59,9 +59,9 @@ void SenderApplication::didReceiveGoodPacket(SerialPacket *p) {
     if (_receivedCommand.ack == STATUS_ACK) {
         // this packet got acknowledgement
         if (_receivedCommand.serial == _currentCommand.serial) {
-            Serial.println("Ack " + String((uint32_t)_currentCommand.serial, DEC) + "!");
+            Serial.println("ACK " + String((uint32_t)_currentCommand.serial, DEC) + " OK!");
         } else {
-            Serial.println("Ack " + String((uint32_t)_receivedCommand.serial, DEC) + " (OoS! Expecting " + String((uint32_t)_currentCommand.serial, DEC) + ")");
+            Serial.println("ACK " + String((uint32_t)_receivedCommand.serial, DEC) + " OoS. Expected " + String((uint32_t)_currentCommand.serial, DEC) + ")");
         }
     } else {
         Serial.println("ACK not request for this packet:");
@@ -86,19 +86,19 @@ void SenderApplication::didReceiveBadPacket(SerialPacket *p, uint8_t err) {
     Serial.print(err, DEC);
     Serial.print(": ");
     switch (err) {
-        case Packet::ERROR_CRC:
+        case SerialPacket::ERROR_CRC:
             Serial.print("CRC Mismatch");
             break;
-        case Packet::ERROR_FRAME:
+        case SerialPacket::ERROR_FRAME:
             Serial.print("Framing (missing end)");
             break;
-        case Packet::ERROR_LENGTH:
+        case SerialPacket::ERROR_LENGTH:
             Serial.print("Data Length");
             break;
-        case Packet::ERROR_OVERFLOW:
+        case SerialPacket::ERROR_OVERFLOW:
             Serial.print("Buffer Overflow");
             break;
-        case Packet::ERROR_TIMEOUT:
+        case SerialPacket::ERROR_TIMEOUT:
             Serial.print("Timeout");
             break;
             
@@ -108,6 +108,7 @@ void SenderApplication::didReceiveBadPacket(SerialPacket *p, uint8_t err) {
     }
     Serial.println(" ");
     _state = STATE_READY;
+    delay(250); // keep red led on a bit
     digitalWrite(LED_BAD, LOW);
     delay(3000);
 }
@@ -140,8 +141,7 @@ void SenderApplication::main() {
     SerialPacket p;
     p.setDelegate(this);
     p.setTimeout(2000);
-    p.sendUsing(&Serial1);
-    p.receiveUsing(&Serial1);
+    p.use(&Serial1);
 
     _currentCommand.serial = 0;
 
@@ -160,19 +160,21 @@ void SenderApplication::main() {
             _newPacket();
             uint8_t bytesSent = p.send((uint8_t *)&_currentCommand, sizeof(_currentCommand));
             if (bytesSent > 0) {
+                digitalWrite(LED_GOOD, HIGH);
                 Serial.print("OK: Sent " + String(bytesSent, DEC) + " bytes: ");
                 Serial.print(" Dev:"); Serial.print(_currentCommand.device, DEC);
                 Serial.print(", Cmd:"); Serial.print(_currentCommand.command, DEC);
                 Serial.print(", Val:"); Serial.print(_currentCommand.value, DEC);
                 Serial.print(", Ser:"); Serial.print((unsigned long)_currentCommand.serial, DEC);
                 Serial.print(", Ack:"); Serial.println(_currentCommand.ack == STATUS_ACK ? "Y" : "N");
+                digitalWrite(LED_GOOD, LOW);
                 if (_currentCommand.ack == STATUS_NACK) {
                     _state = STATE_WAIT_ACK;
                     p.startReceiving();
                 }
             } else {
-                Serial.println("ERR: Bytes sent (" + String(bytesSent, DEC) + ") mismatch error.");
                 digitalWrite(LED_BAD, HIGH);
+                Serial.println("ERR: Bytes sent (" + String(bytesSent, DEC) + ") mismatch error.");
                 delay(100);
                 digitalWrite(LED_BAD, LOW);
             }
